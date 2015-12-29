434298cade66cfd3af3f5169730112b10e7d1cba
Nithin Philips <nithin@nithinphilips.com>
2015-12-29 11:45:09 -0500

    Add script to run parser.

---
+++ axiom-parser-runner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+"""Convenience wrapper for running axiom directly from source tree."""
+
+
+from axiom.axiom import eventparser_entry
+
+
+if __name__ == '__main__':
+    eventparser_entry()
