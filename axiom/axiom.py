ef520353f48ea606dd1786612bd53db3579aa15f
Nithin Philips <nithin@nithinphilips.com>
2016-01-04 12:06:52 -0500

    Add a --version argument.

--- axiom/axiom.py
+++ axiom/axiom.py
@@ -23,6 +23,10 @@ COMMON_PARSER.add_argument('--debug',
                            action='store_true',
                            default=False,
                            help="Enable debug logging.")
+COMMON_PARSER.add_argument('--version',
+                           action='store_true',
+                           default=False,
+                           help="Print version information and quit.")
 
 def schedulevalidator_entry():
     parser = ArghParser(parents=[COMMON_PARSER])
@@ -36,6 +40,10 @@ def schedulevalidator_entry():
             level=logging.DEBUG,
             format='%(asctime)s %(levelname)s: %(message)s'
         )
+
+    if args.version:
+        print_version()
+        return
 
     parser.dispatch()
 
@@ -52,5 +60,29 @@ def eventparser_entry():
             format='%(asctime)s %(levelname)s: %(message)s'
         )
 
+    if args.version:
+        print_version()
+        return
+
     parser.dispatch()
 
+def print_version():
+    sys.stdout.write(
+            "Axiom. Tool to validate TRIRIGA PM Schedules.\n"
+            "Version {}\n"
+            "\n"
+            "Copyright (C) 2016 Nithin Philips\n"
+            "\n"
+            "This program is free software: you can redistribute it and/or modify\n"
+            "it under the terms of the GNU General Public License as published by\n"
+            "the Free Software Foundation, either version 3 of the License, or\n"
+            "(at your option) any later version.\n"
+            "\n"
+            "This program is distributed in the hope that it will be useful,\n"
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+            "GNU General Public License for more details.\n"
+            "\n"
+            "You should have received a copy of the GNU General Public License\n"
+            "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n".format(__version__)
+    )
